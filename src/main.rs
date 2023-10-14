20a3ce4be638ae07212d4bb9a63edccfc2fb16eb
AnonymousAcid <anonymousacid14@gmail.com>
2023-10-13 17:18:45 -0700

    trying to make custom shader

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,4 @@
-use bevy::{
-    DefaultPlugins,
-    prelude::App,
-};
+use bevy::{prelude::*, render::render_resource::AsBindGroup, reflect::{TypePath, TypeUuid}, utils::Uuid};
 use utils::LogFramesPlugin;
 
 mod utils;
@@ -11,8 +8,51 @@ fn main() {
 
     app.add_plugins((
         DefaultPlugins,
-        LogFramesPlugin::default()
+        LogFramesPlugin::default(),
+        MaterialPlugin::<CustomMaterial>::default()
     ));
 
     app.run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<CustomMaterial>>,
+    asset_server: Res<AssetServer>,
+) {
+    // cube
+    commands.spawn(MaterialMeshBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+        transform: Transform::from_xyz(0.0, 0.5, 0.0),
+        material: materials.add(CustomMaterial {
+            color: Color::BLUE,
+            alpha_mode: AlphaMode::Blend,
+        }),
+        ..default()
+    });
+
+    // camera
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
+}
+
+impl Material for CustomMaterial {
+    fn fragment_shader() -> bevy::render::render_resource::ShaderRef {
+        "./shaders/custom_material.wgsl".into()
+    }
+
+    fn alpha_mode(&self) -> AlphaMode {
+        self.alpha_mode
+    }
+}
+
+#[derive(TypePath, AsBindGroup, Debug, Clone, TypeUuid)]
+#[uuid = "1f73101b-0d10-4c34-8aa9-f0e2b3b77ec2"]
+pub struct CustomMaterial {
+    #[uniform(0)]
+    color: Color,
+    alpha_mode: AlphaMode
 }
