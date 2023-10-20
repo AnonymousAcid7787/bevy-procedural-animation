259b5782fdd479e261d246ba53f0eb49c234fd18
AnonymousAcid <anonymousacid14@gmail.com>
2023-10-20 15:32:15 -0700

    make body (WIP)

--- src/main.rs
+++ src/main.rs
@@ -44,34 +44,37 @@ fn main() {
 }
 
 macro_rules! spawn_mesh {
-    ($mesh:expr, $x:expr, $y:expr, $z:expr, $commands:expr, $mats:expr, $meshes:expr) => {
+    ($mesh:expr, $shape:expr, $x:expr, $y:expr, $z:expr, $commands:expr, $material:expr, $transform:expr) => {
         unsafe {
             $commands.spawn((
                 PbrBundle {
-                    material: $mats.add(Color::PURPLE.into()),
-                    mesh: $meshes.add($mesh.clone().into()),
-                    transform: Transform::from_xyz($x, $y, $z),
+                    mesh: $mesh, 
+                    material: $material,
+                    transform: $transform,
                     ..Default::default()
                 },
-                std::mem::transmute::<shape::Capsule, TestComponent>($mesh)
-            ));
+                std::mem::transmute::<shape::Capsule, TestComponent>($shape)
+            ))
         }
     };
 }
 
 fn setup(
     mut commands: Commands,
-    mut materials: ResMut<Assets<TestMaterial>>,
     mut meshes: ResMut<Assets<Mesh>>,
     mut standard_materials: ResMut<Assets<StandardMaterial>>,
-    asset_server: Res<AssetServer>
 ) {
-    let material = materials.add(TestMaterial {
-        color: Color::PURPLE,
-        color_texture: Some(asset_server.load("images/icon.png")), 
-        alpha_mode: AlphaMode::Blend
-    });
+    let material = standard_materials.add(Color::PURPLE.into());
 
+    let radius = 0.03_f32;
+    let torso_depth = 0.6_f32;
+    let torso_len = torso_depth + radius*2.;
+    let arm_depth = torso_depth;
+    let arm_len = arm_depth + radius*2.;
+    let leg_depth = 0.75;
+    let leg_len = leg_depth + radius*2.;
+
+    //shapes
     let torso = shape::Capsule {
         radius: 0.03,
         depth: 0.60,
@@ -90,7 +93,78 @@ fn setup(
         ..Default::default()
     };
 
-    spawn_mesh!(arm, 0., 0., 0., commands, standard_materials, meshes);
+    //transforms
+    let mut arm1_transform = Transform::from_xyz(0., 0., 0.);
+        arm1_transform.rotate_around(
+            Vec3::new(0., 0.3, 0.), 
+            Quat::from_axis_angle(Vec3::Z, 45_f32.to_radians())
+        );
+    let mut arm2_transform = Transform::from_xyz(0., 0., 0.);
+        arm2_transform.rotate_around(
+            Vec3::new(0., 0.3, 0.), 
+            Quat::from_axis_angle(Vec3::Z, -45_f32.to_radians())
+        );
+    let torso_transform = Transform::from_xyz(0., 0., 0.);
+    let mut leg1_transform = Transform::from_xyz(0., -torso_len, 0.);
+        leg1_transform.rotate_around(
+            Vec3::new(0., -torso_len + leg_len/2., 0.), 
+            Quat::from_axis_angle(Vec3::Z, 30_f32.to_radians())
+        );
+    let mut leg2_transform = Transform::from_xyz(0., -torso_len, 0.);
+        leg2_transform.rotate_around(
+            Vec3::new(0., -torso_len + leg_len/2., 0.), 
+            Quat::from_axis_angle(Vec3::Z, -30_f32.to_radians())
+        );
+
+    
+    //spawning entities
+    let mut arm1_entity = spawn_mesh!(
+        meshes.add(arm.into()), 
+        arm, 
+        -1., 0., 0., 
+        commands, 
+        material.clone(),
+        arm1_transform
+    );
+    let mut arm2_entity = spawn_mesh!(
+        meshes.add(arm.into()), 
+        arm, 
+        1., 0., 0., 
+        commands, 
+        material.clone(),
+        arm2_transform
+    );
+ 
+    let mut torso_entity =spawn_mesh!(
+        meshes.add(torso.into()), 
+        torso, 
+        0., 0., 0., 
+        commands, 
+        material.clone(),
+        torso_transform
+    );
+ 
+    let mut leg1_entity = spawn_mesh!(
+        meshes.add(leg.into()), 
+        leg, 
+        -1., -0.6, 0., 
+        commands, 
+        material.clone(),
+        leg1_transform
+    );
+    let mut leg2_entity = spawn_mesh!(
+        meshes.add(leg.into()), 
+        leg, 
+        1., -0.6, 0., 
+        commands, 
+        material.clone(),
+        leg2_transform
+    );
+
+    //legs
+    //  leg2 rotate 45deg z axis
+    //  leg1 rot -45deg z axis
+
 
     //flycam
     commands.spawn((
