8eb339e4dcc1795b23401b8591c81e2c44136b4e
AnonymousAcid <anonymousacid14@gmail.com>
2023-10-21 19:34:43 -0700

    remove TestComponent stuff

--- src/main.rs
+++ src/main.rs
@@ -229,16 +229,9 @@ fn stickman_mesh_setup(
 }
 
 fn test_update(
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut capsules: Query<(&TestComponent, &mut Handle<Mesh>)>
+    
 ) {
-    for (capsule, mut handle) in capsules.iter_mut() {
-        meshes.remove(handle.clone());
-        unsafe {
-            let new_handle = meshes.add(std::mem::transmute_copy::<TestComponent, shape::Capsule>(capsule).into());
-            let _ = std::mem::replace(handle.as_mut(), new_handle);
-        }
-    }
+    
 }
 
 #[derive(Default, Bundle)]
@@ -250,18 +243,7 @@ pub struct StickmanMeshParentBundle {
 
 #[derive(Component, Reflect)]
 pub struct TestComponent {
-    /// Radius on the `XZ` plane.
-    pub radius: f32,
-    /// Number of sections in cylinder between hemispheres.
-    pub rings: usize,
-    /// Height of the middle cylinder on the `Y` axis, excluding the hemispheres.
-    pub depth: f32,
-    /// Number of latitudes, distributed by inclination. Must be even.
-    pub latitudes: usize,
-    /// Number of longitudes, or meridians, distributed by azimuth.
-    pub longitudes: usize,
     
-    uv_profile: i8,
 }
 
 // This is the struct that will be passed to your shader
